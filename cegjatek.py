5b23f5526712803805789f0237905070c4ddfe0c
H.Mia <100997921+H-Miaa@users.noreply.github.com>
2025-05-21 21:50:52 +0200

    bolt elkészítése

--- cegjatek.py
+++ cegjatek.py
@@ -17,3 +17,25 @@ def allapot_kiiras():
     print("Készleted:")
     for anyag, db in keszlet.items():
         print(f"- {anyag}: {db} db")
+
+def bolt():
+    global penz
+    print("\nBolt – anyagvásárlás")
+    for anyag, ar in anyag_arak.items():
+        print(f"{anyag}: {ar} Ft/db")
+    valasztott_anyag = input("Melyik anyagot szeretnéd megvenni? (PLA/ABS/PETG): ").strip().upper()
+    if valasztott_anyag in anyag_arak:
+        try:
+            db_szam = int(input("Hány darabot szeretnél vásárolni?: "))
+        except ValueError:
+            print('Hibás darabszám.')
+            return
+        osszeg = db_szam * anyag_arak[valasztott_anyag]
+        if osszeg <= penz:
+            keszlet[valasztott_anyag] += db_szam
+            penz -= osszeg
+            print(f"Sikeresen vásároltál {db_szam} db {valasztott_anyag} anyagot.")
+        else:
+            print('Nincs elég pénzed ehhez a vásárláshoz.')
+    else:
+        print("Ismeretlen anyag.")
